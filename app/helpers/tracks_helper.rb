6b241ceb7226f51f551087605243570603897867
Niklas Holmgren <niklas.holmgren@mindset.se>
2009-10-20 18:33:41 +0200

    Fixed the TracksHelper#seconds_to_time helper to remove extra leading zero when less than 60 seconds.

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,10 +1,12 @@
 module TracksHelper
   
   def seconds_to_time(seconds)
-    if seconds > 1.hour
-      Time.at(seconds).gmtime.strftime('%R:%S').gsub(/^0([1-9]+)/,'\1')
+    if seconds < 1.minute
+      "0:#{seconds.to_i}"
+    elsif seconds < 1.hour
+      Time.at(seconds.to_i).gmtime.strftime('%M:%S').gsub(/^0([1-9]+)/,'\1')
     else
-      Time.at(seconds).gmtime.strftime('%M:%S').gsub(/^0([1-9]+)/,'\1')
+      Time.at(seconds.to_i).gmtime.strftime('%R:%S').gsub(/^0([1-9]+)/,'\1')
     end
   end
   
--- test/unit/helpers/tracks_helper_test.rb
+++ test/unit/helpers/tracks_helper_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class TracksHelperTest < ActionView::TestCase
+  
+  test "seconds to time should return time without leading zero, unless less than 60 seconds" do
+    assert_equal '1:01', seconds_to_time(1.minute + 1.second)
+    assert_equal '0:59', seconds_to_time(59.seconds)
+    assert_equal '1:00:00', seconds_to_time(1.hour)
+  end
+  
 end
