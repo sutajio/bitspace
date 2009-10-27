41045ba5b0c655dbe343407c7c5138cf78e897d4
Niklas Holmgren <niklas.holmgren@mindset.se>
2009-10-27 11:34:37 +0100

    Added a tooltip to releases on the "Years" page.

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -1,2 +1,11 @@
 module ReleasesHelper
+  
+  def release_title(release)
+    if release.year.present?
+      "#{h(release.artist.name)} &ndash; #{h(release.title)} (#{release.year})"
+    else
+      "#{h(release.artist.name)} &ndash; #{h(release.title)}"
+    end
+  end
+  
 end
