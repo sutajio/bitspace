fd2175577131212e61b5c85e646e97700af4d3b7
Niklas Holmgren <niklas@bitspace.se>
2009-10-30 22:17:45 +0100

    Don't fetch more than 10 releases per year on the "Years" page.

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,13 +1,13 @@
 class YearsController < ApplicationController
   
   def index
-    @years = ((Time.now.year - 10)..Time.now.year).to_a.reverse.collect {|year| [year, Release.by_year(year)] }
-    @years << ['2000-1990', Release.by_year(1990..2000)]
-    @years << ['1990-1980', Release.by_year(1980..1990)]
-    @years << ['1980-1970', Release.by_year(1970..1980)]
-    @years << ['1970-1960', Release.by_year(1960..1970)]
-    @years << ['1960-1950', Release.by_year(1950..1960)]
-    @years << ['1950-1940', Release.by_year(1940..1950)]
+    @years = ((Time.now.year - 10)..Time.now.year).to_a.reverse.collect {|year| [year, Release.by_year(year).all(:limit => 10)] }
+    @years << ['2000-1990', Release.by_year(1990..2000).all(:limit => 10)]
+    @years << ['1990-1980', Release.by_year(1980..1990).all(:limit => 10)]
+    @years << ['1980-1970', Release.by_year(1970..1980).all(:limit => 10)]
+    @years << ['1970-1960', Release.by_year(1960..1970).all(:limit => 10)]
+    @years << ['1960-1950', Release.by_year(1950..1960).all(:limit => 10)]
+    @years << ['1950-1940', Release.by_year(1940..1950).all(:limit => 10)]
   end
   
   def show
