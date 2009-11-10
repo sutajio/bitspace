2d5c6bb470a1c0448a03a1ec030d7b7e9ec751fd
Fredrik Lundqvist <fredrik@bitspace.se>
2009-11-10 23:05:53 +0100

    Made style changes to the top navigation and updated the facebook style for comments.

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,6 +1,6 @@
 module UserSessionsHelper
   
-  def facebook_login_button(button_text = 'Login with Facebook')
+  def facebook_login_button(button_text = 'Sign in with Facebook')
     "<fb:login-button v=\"2\" size=\"medium\" onlogin=\"connect_to_facebook()\">#{button_text}</fb:login-button>" +
     form_tag(user_sessions_path, :method => :post, :id => 'connect-to-facebook-form') + '</form>' +
     "<script type=\"text/javascript\" charset=\"utf-8\">
