43070e17283b507e50682fa432414248a543b8b2
Niklas Holmgren <niklas@bitspace.se>
2009-11-12 17:54:01 +0100

    Made some minor style adjustments to the dashboard.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,7 @@ class DashboardsController < ApplicationController
     @latest = current_user.releases.all(
       :limit => 3,
       :order => 'created_at DESC',
-      :conditions => ['artwork_file_size > ?', 40.kilobytes])
+      :conditions => ['artwork_file_size > ?', 30.kilobytes])
   end
   
 end
