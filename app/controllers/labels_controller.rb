93ff4dfc722755e135cf31e1f2627959b35000b9
Niklas Holmgren <niklas@bitspace.se>
2009-11-24 14:47:20 +0100

    Added pagination for the Labels-page.

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,7 +1,12 @@
 class LabelsController < ApplicationController
   
   def index
-    @labels = current_user.labels.search_for(params[:q]).paginate(:page => params[:page])
+    @labels = current_user.labels.search_for(params[:q]).paginate(
+        :page => params[:page],
+        :per_page => 10)
+    if request.xhr? && @labels.empty?
+      render :nothing => true
+    end
   end
   
   def show
