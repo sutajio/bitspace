a98bd42dd1367e8d90b530fe663bf7477bcab027
Niklas Holmgren <niklas@bitspace.se>
2009-11-25 17:43:31 +0100

    Allow releases without artwork to be shown on the dashboard.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,8 +3,7 @@ class DashboardsController < ApplicationController
   def show
     @latest = current_user.releases.all(
       :limit => 3,
-      :order => 'created_at DESC',
-      :conditions => ['artwork_file_size > 0'])
+      :order => 'created_at DESC')
   end
   
 end
