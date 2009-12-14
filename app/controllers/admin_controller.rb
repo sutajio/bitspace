2efd580455155f8eb25cd6d0a79583ef903e24e3
Niklas Holmgren <niklas@bitspace.se>
2009-12-14 22:19:08 +0100

    Order the jobs on the admin/jobs page after created_at instead of run_at.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,7 @@ class AdminController < ApplicationController
   end
 
   def jobs
-    @jobs = Delayed::Job.paginate(:page => params[:page], :order => 'run_at')
+    @jobs = Delayed::Job.paginate(:page => params[:page], :order => 'created_at DESC')
   end
 
   def run_job
