581ef618207d81c76f9ad55d6ba47c23ca723f66
Niklas Holmgren <niklas@bitspace.se>
2010-02-10 11:51:22 +0100

    Made the Years-page shorter and easier to scan through and faster to render.

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,13 +1,13 @@
 class YearsController < ApplicationController
   
   def index
-    @years = (2000..Time.now.year).to_a.reverse.collect {|year| [year, current_user.releases.by_year(year).paginate(:page => 1, :per_page => 12)] }
-    @years << ['1990-1999', current_user.releases.by_year(1990..1999).paginate(:page => 1, :per_page => 12)]
-    @years << ['1980-1989', current_user.releases.by_year(1980..1989).paginate(:page => 1, :per_page => 12)]
-    @years << ['1970-1979', current_user.releases.by_year(1970..1979).paginate(:page => 1, :per_page => 12)]
-    @years << ['1960-1969', current_user.releases.by_year(1960..1969).paginate(:page => 1, :per_page => 12)]
-    @years << ['1950-1959', current_user.releases.by_year(1950..1959).paginate(:page => 1, :per_page => 12)]
-    @years << ['<1950', current_user.releases.by_year(0..1949).paginate(:page => 1, :per_page => 12)]
+    @years = (2000..Time.now.year).to_a.reverse.collect {|year| [year, current_user.releases.by_year(year).paginate(:page => 1, :per_page => 6)] }
+    @years << ['1990-1999', current_user.releases.by_year(1990..1999).paginate(:page => 1, :per_page => 6)]
+    @years << ['1980-1989', current_user.releases.by_year(1980..1989).paginate(:page => 1, :per_page => 6)]
+    @years << ['1970-1979', current_user.releases.by_year(1970..1979).paginate(:page => 1, :per_page => 6)]
+    @years << ['1960-1969', current_user.releases.by_year(1960..1969).paginate(:page => 1, :per_page => 6)]
+    @years << ['1950-1959', current_user.releases.by_year(1950..1959).paginate(:page => 1, :per_page => 6)]
+    @years << ['<1950', current_user.releases.by_year(0..1949).paginate(:page => 1, :per_page => 6)]
   end
   
   def show
