8258208ab854e59fbbdc9a26de577e4ab97fe60c
Niklas Holmgren <niklas@bitspace.se>
2010-02-11 17:55:59 +0100

    Enforce the archived status on years, labels and artists pages.

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,13 +1,13 @@
 class YearsController < ApplicationController
   
   def index
-    @years = (2000..Time.now.year).to_a.reverse.collect {|year| [year, current_user.releases.by_year(year).paginate(:page => 1, :per_page => 6)] }
-    @years << ['1990-1999', current_user.releases.by_year(1990..1999).paginate(:page => 1, :per_page => 6)]
-    @years << ['1980-1989', current_user.releases.by_year(1980..1989).paginate(:page => 1, :per_page => 6)]
-    @years << ['1970-1979', current_user.releases.by_year(1970..1979).paginate(:page => 1, :per_page => 6)]
-    @years << ['1960-1969', current_user.releases.by_year(1960..1969).paginate(:page => 1, :per_page => 6)]
-    @years << ['1950-1959', current_user.releases.by_year(1950..1959).paginate(:page => 1, :per_page => 6)]
-    @years << ['<1950', current_user.releases.by_year(0..1949).paginate(:page => 1, :per_page => 6)]
+    @years = (2000..Time.now.year).to_a.reverse.collect {|year| [year, current_user.releases.without_archived.by_year(year).paginate(:page => 1, :per_page => 6)] }
+    @years << ['1990-1999', current_user.releases.without_archived.by_year(1990..1999).paginate(:page => 1, :per_page => 6)]
+    @years << ['1980-1989', current_user.releases.without_archived.by_year(1980..1989).paginate(:page => 1, :per_page => 6)]
+    @years << ['1970-1979', current_user.releases.without_archived.by_year(1970..1979).paginate(:page => 1, :per_page => 6)]
+    @years << ['1960-1969', current_user.releases.without_archived.by_year(1960..1969).paginate(:page => 1, :per_page => 6)]
+    @years << ['1950-1959', current_user.releases.without_archived.by_year(1950..1959).paginate(:page => 1, :per_page => 6)]
+    @years << ['<1950', current_user.releases.without_archived.by_year(0..1949).paginate(:page => 1, :per_page => 6)]
   end
   
   def show
@@ -22,7 +22,7 @@ class YearsController < ApplicationController
     else
       years = @year
     end
-    @releases = current_user.releases.by_year(years)
+    @releases = current_user.releases.without_archived.by_year(years)
   end
   
 end
