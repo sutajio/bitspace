c0e8b351b00532e8010aae0dcda577aa866d7201
Niklas Holmgren <niklas@bitspace.se>
2010-02-16 17:12:26 +0100

    Fixed handling of exceptions in SearchesController caused by invalid queries.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,14 @@ class SearchesController < ApplicationController
   
   layout nil, :only => [:suggestions]
   
+  rescue_from ScopedSearch::QueryNotSupported do |exception|
+    head :no_content
+  end
+  
+  rescue_from NoMethodError do |exception|
+    head :no_content
+  end
+  
   def show
     @releases = current_user.releases.search_for(params[:q]).paginate(:page => params[:page], :include => [:artist, :tracks])
   end
