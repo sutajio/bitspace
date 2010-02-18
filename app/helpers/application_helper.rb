e709a4dc0c809acd08c00e07bf91220414d3a78c
Niklas Holmgren <niklas@bitspace.se>
2010-02-18 21:29:21 +0100

    Fixed bug in ApplicationHelper#more. I think...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@
 module ApplicationHelper
   
   def more(collection)
+    return if collection.blank?
     link_to('...', polymorphic_path(collection.first.class.new, :page => params[:page].present? ? params[:page].to_i + 1 : 2), :rel => 'more', :class => 'more', :target => '_self')
   end
   
