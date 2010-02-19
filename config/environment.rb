a858c294a82ef4981790d46f384ab3b738e31924
Niklas Holmgren <niklas@bitspace.se>
2010-02-19 12:11:33 +0100

    Use the Northscale memcache add-on.

--- config/environment.rb
+++ config/environment.rb
@@ -37,6 +37,7 @@ Rails::Initializer.run do |config|
   config.gem 'flacinfo-rb', :lib => 'flacinfo', :source => 'http://gemcutter.org'
   config.gem 'bluecloth', :source => 'http://gemcutter.org'
   config.gem 'rbrainz', :source => 'http://gemcutter.org'
+  config.gem 'memcache-auth', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ config.action_view.cache_template_loading            = true
 # config.logger = SyslogLogger.new
 
 # Use a different cache store in production
-# config.cache_store = :mem_cache_store
+require 'memcache-auth'
+config.cache_store = :mem_cache_store, MemcacheAuth::Rails.new
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
