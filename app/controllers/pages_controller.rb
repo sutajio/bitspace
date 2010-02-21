8ec303e8d49342d12e8e54e1d52641b38b9b828d
Niklas Holmgren <niklas@bitspace.se>
2010-02-22 00:17:01 +0100

    Fixed another typo.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   
   def index
     @page_title = 'Music Backup, Music Organizer, HTML5 Music Player'
-    @page_description = 'Keep your music in the cloud with Bitspace. Upload your collection and enjoy it everyday, from anywhere. The best way to backup your music.'
+    @page_description = 'Keep your music in the cloud with Bitspace. Upload your collection and enjoy it every day, from anywhere. The best way to backup your music.'
     @page_keywords = 'music backup, music organizer, music library, html5 music player, audio tag, id3 tags, musicbrainz, last.fm, audioscrobbler, discogs'
   end
   
