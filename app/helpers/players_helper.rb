cec6d379873d1fee5531d244333a0a5c22ba6c7a
Niklas Holmgren <niklas@bitspace.se>
2010-04-20 11:03:47 +0200

    Use Flash for audio playback on Mac OS X 10.6 because of bug in Quicktime X (see https://bugs.webkit.org/show_bug.cgi?id=32159).

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -2,7 +2,9 @@ module PlayersHelper
   
   def use_html5_audio?
     user_agent = request.headers['User-Agent'] || ''
-    true if user_agent.include?('AppleWebKit')
+    return false if user_agent.match(/Mac OS X 10_6/)
+    return true if user_agent.include?('AppleWebKit')
+    return false
   end
   
 end
