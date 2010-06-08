e99fe261e0a4b8f4e2748b2bb24058fd165fb5aa
Niklas Holmgren <niklas@bitspace.se>
2010-06-08 11:43:56 +0200

    Enabled HTML5 audio for Safari 5.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -2,6 +2,8 @@ module PlayersHelper
   
   def use_html5_audio?
     user_agent = request.headers['User-Agent'] || ''
+    return true if user_agent.include?('Version/5') &&
+                   user_agent.include?('Safari')
     return false if user_agent.match(/Mac OS X 10_6/)
     return true if user_agent.include?('AppleWebKit')
     return false
