595e10af64476ac6d40059f69335d18b5e36fb42
Niklas Holmgren <niklas.holmgren@bukowskis.com>
2011-03-04 17:18:00 +0100

    Moved the scoped_search plugin to a gem.

--- config/environment.rb
+++ config/environment.rb
@@ -38,6 +38,7 @@ Rails::Initializer.run do |config|
   config.gem 'memcache-auth', :source => 'http://gemcutter.org'
   config.gem 'rfeedfinder', :source => 'http://gemcutter.org'
   config.gem 'feed-normalizer', :source => 'http://gemcutter.org'
+  config.gem 'scoped_search', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
