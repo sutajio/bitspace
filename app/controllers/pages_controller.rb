3263a841a61c9fd7e9309bd4c15857564ffb276c
Niklas Holmgren <niklas@sutajio.se>
2011-05-18 16:53:09 +0200

    Removed a bunch of files and updated start page for new Create Digger project.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,56 +5,24 @@ class PagesController < ApplicationController
   
   def index
     response.headers['Cache-Control'] = 'public, max-age=300'
-    @page_title = 'Bitspace — Your music in the cloud'
+    @page_title = 'Crate Digger — Your music in the cloud'
     @page_description = 'Bitspace is a digital music service that lets you upload your own music to the cloud.'
     @page_keywords = 'music backup, music organizer, music library, html5 music player, audio tag, id3 tags, musicbrainz, last.fm, audioscrobbler, discogs'
   end
-  
-  def price
-    redirect_to '/#/price', :status => :moved_permanently
-  end
-  
-  def tour
-    response.headers['Cache-Control'] = 'public, max-age=300'
-    @page_title = 'Take a Look Inside, Product Tour'
-  end
-  
-  def about
-    redirect_to '/#/about', :status => :moved_permanently
-  end
-  
+
   def terms
     response.headers['Cache-Control'] = 'public, max-age=300'
     @page_title = 'Terms and Conditions'
   end
-  
+
   def download
     response.headers['Cache-Control'] = 'public, max-age=300'
     @page_title = 'Download the Mac OS X Client'
     @clients = Client.all
   end
-  
+
   def appstore
     redirect_to 'http://itunes.apple.com/us/app/bitspace/id386505557?mt=8&uo=4'
   end
-  
-  def press
-    response.headers['Cache-Control'] = 'public, max-age=300'
-    @page_title = 'Press kit for Bitspace'
-  end
-  
-  def support
-    redirect_to 'http://getsatisfaction.com/bitspace'
-  end
-  
-  def atp
-    response.headers['Cache-Control'] = 'public, max-age=300'
-    @page_title = 'Bitspace at All Tomorrow\'s Parties Curated by Matt Groening'
-  end
-  
-  def featured
-    response.headers['Cache-Control'] = 'public, max-age=300'
-    render :action => 'featured', :layout => false
-  end
-  
+
 end
